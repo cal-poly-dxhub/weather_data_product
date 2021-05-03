ee79aaac4106092c0e26c16e189101cfe19da9a3
chasepeak <chasempeak@gmail.com>
2021-05-03 13:06:45 -0700

    table definition for temperature profiler

--- database/db_config.sql
+++ database/db_config.sql
@@ -29,6 +29,9 @@ CREATE TABLE TowerMeasurements (
     PRIMARY KEY (TowerId, MeasurementDate, MeasurementTime)
 );
 
+#optimize data types where applicable
+#abstract MxHeight->WNoise attributes to separate table to reduce data duplication? foreign key: (AssetId, MeasurementDateTime)
+
 CREATE TABLE MiniSODARMeasurements (
     AssetId INT, #(integer) (nnnn)
     
@@ -36,7 +39,7 @@ CREATE TABLE MiniSODARMeasurements (
     
     MxHeight INT, #Mixing Height in meters (int) (nnn) [ID 0001]
     
-    UNoise INT, #Noise level of the U measurement in millivolts (int) (nnnn) [ID 0002}
+    UNoise INT, #Noise level of the U measurement in millivolts (int) (nnnn) [ID 0002]
     
     VNoise INT, #Noise level of the V measurement in millivolts (int) (nnnn) [ID 0003]
     
@@ -64,7 +67,7 @@ CREATE TABLE MiniSODARMeasurements (
 
     GDIR_QCFLAG INT,
 
-    W DOUBLE(3,2) signed, #Vertical wind component in meters per second (signed floating point) (sn.nn)
+    W DOUBLE(3,2) SIGNED, #Vertical wind component in meters per second (signed floating point) (sn.nn)
 
     W_QCFLAG INT,
 
@@ -84,7 +87,7 @@ CREATE TABLE MiniSODARMeasurements (
 
     SNRW_QCFLAG INT,
 
-    U DOUBLE(4,2) signed, #East West wind component in meters per second (signed floating point) (snn.nn)
+    U DOUBLE(4,2) SIGNED, #East West wind component in meters per second (signed floating point) (snn.nn)
 
     U_QCFLAG INT,
 
@@ -104,7 +107,7 @@ CREATE TABLE MiniSODARMeasurements (
 
     SNRU_QCFLAG INT,
 
-    V DOUBLE(4,2) signed, #North South wind component in meters per second (signed floating point) (snn.nn)
+    V DOUBLE(4,2) SIGNED, #North South wind component in meters per second (signed floating point) (snn.nn)
 
     V_QCFLAG INT,
 
@@ -134,4 +137,60 @@ CREATE TABLE MiniSODARMeasurements (
 
 
     PRIMARY KEY (AssetId, MeasurementDateTime, GateNum)
+);
+
+
+CREATE TABLE TemperatureProfilerMeasurements (
+    AssetId INT, #(integer) (nnnn)
+
+    MeasurementDateTime DATETIME, #(string) (dd/mm/yyyy hh:mm:ss)
+
+    GateNum INT, #The number of this gate (integer) (nn)
+
+    HT DOUBLE(5,3), #Sample height in Kilometers (floating point) (nn.nnn)
+
+    HT_QCFLAG INT,
+
+    T DOUBLE(3,1) SIGNED, #Temperature in degrees Celsius (signed floating point) (snn.n)
+
+    T_QCFLAG INT,
+
+    Tc DOUBLE(3,1) SIGNED, #Corrected Temperature in degrees Celsius (signed floating point) (snn.n)
+
+    Tc_QCFLAG INT,
+
+    W DOUBLE(3,1) SIGNED, #Vertical wind component in meters per second (signed floating point) (snn.n)
+
+    W_QCFLAG INT,
+
+    SNRT INT SIGNED, #Signal to Noise Ratio of T in dB (signed integer) (snn)
+
+    SNRT_QCFLAG INT,
+
+    SNRTc INT SIGNED, #Signal to Noise Ratio of Tc in dB (signed integer) (snn)
+
+    SNRTc_QCFLAG INT,
+
+    SNRW INT SIGNED, #Signal to Noise Ratio of the W component in dB (signed integer) (snn)
+
+    SNRW_QCFLAG INT,
+
+    CNT-T INT, #Number of Temperature observations in this consensus (integer) (nn)
+
+    CNT-T_QCFLAG INT,
+
+    CNT-Tc INT, #Number of Corrected Temperature observations in this consensus (integer) (nn)
+
+    CNT-Tc_QCFLAG INT,
+
+    CNT-W INT, #Number of W observations in this consensus (integer) (nn)
+
+    CNT-W_QCFLAG INT,
+
+    PRIMARY KEY (AssetId, MeasurementDateTime, GateNum)
+);
+
+
+CREATE TABLE WindProfilerMeasurements (
+    
 );
