8f1050c8eacfea6f2eeee7df33e8b038746365d8
chasepeak <chasempeak@gmail.com>
2021-05-03 12:50:01 -0700

    update QC_FLAGs + update data types

--- database/db_config.sql
+++ database/db_config.sql
@@ -29,7 +29,6 @@ CREATE TABLE TowerMeasurements (
     PRIMARY KEY (TowerId, MeasurementDate, MeasurementTime)
 );
 
-#double check data types
 CREATE TABLE MiniSODARMeasurements (
     AssetId INT, #(integer) (nnnn)
     
@@ -47,47 +46,91 @@ CREATE TABLE MiniSODARMeasurements (
     
     HT INT, #Sample height in meters (integer) (nnn)
 
+    HT_QCFLAG INT,
+
     SPD DOUBLE(4, 2), #Speed in meters per second (floating point) (nn.nn)
+
+    SPD_QCFLAG INT,
 
     DIR INT, #Direction in degrees (integer) (nnn)
 
+    DIR_QCFLAG INT,
+
     GSPD DOUBLE(4, 2), #Gust speed in meters per second (floating point) (nn.nn)
+
+    GSPD_QCLFAG INT,
 
     GDIR INT, #Gust Direction in degrees (integer) (nnn)
 
-    W DOUBLE(4,2), #Vertical wind component in meters per second (signed floating point) (sn.nn)
+    GDIR_QCFLAG INT,
+
+    W DOUBLE(3,2) signed, #Vertical wind component in meters per second (signed floating point) (sn.nn)
+
+    W_QCFLAG INT,
 
     SDW DOUBLE(4,2), #1 minute Standard deviation of W in meters per second (floating point) (nn.nn)
 
+    SDW_QCFLAG INT,
+
     NW INT, #Number of W observations in the sample (integer) (nn)
+
+    NW_QCFLAG INT,
 
     IW INT, #Signal return intensity in milivolts (integer) (nnnn)
 
+    IW_QCFLAG INT,
+
     SNRW INT, #Signal to Noise Ratio of the W component no units (integer) (nnnn)
 
-    U DOUBLE(4,2), #East West wind component in meters per second (signed floating point) (snn.nn)
+    SNRW_QCFLAG INT,
+
+    U DOUBLE(4,2) signed, #East West wind component in meters per second (signed floating point) (snn.nn)
+
+    U_QCFLAG INT,
 
     SDU DOUBLE(4,2), #Standard deviation of U in meters per second (floating point) (nn.nn)
 
+    SDU_QCFLAG INT,
+
     NU INT, #Number of U observations in the sample (integer) (nn)
+
+    NU_QCFLAG INT,
 
     IU INT, #Signal return intensity in milivolts (integer) (nnnn)
 
+    IU_QCFLAG INT,
+
     SNRU INT, #Signal to Noise Ratio of the U component no units (integer) (nnnn)
 
-    V DOUBLE(4,2), #North South wind component in meters per second (signed floating point) (snn.nn)
+    SNRU_QCFLAG INT,
+
+    V DOUBLE(4,2) signed, #North South wind component in meters per second (signed floating point) (snn.nn)
+
+    V_QCFLAG INT,
 
     SDV DOUBLE(4,2), #Standard deviation of V in meters per second (floating point) (nn.nn)
 
+    SDV_QCFLAG INT,
+
     NV INT, #Number of V observations in the sample (integer) (nn)
+
+    NV_QCFLAG INT,
 
     IV INT, #Signal return intensity in milivolts (integer) (nnnn)
 
+    IV_QCFLAG INT,
+
     SNRV INT, #Signal to Noise Ratio of the V component no units (integer) (nnnn)
+
+    SNRV_QCFLAG INT,
 
     SDW5 DOUBLE(4,2), #5 minute Standard deviation of W in meters per second (floating point) (nn.nn)
 
+    SDW5_QCFLAG INT,
+
     SDW10 DOUBLE(4,2), #10 minute Standard deviation of W in meters per second (floating point) (nn.nn)
+
+    SDW10_QCFLAG INT,
 
 
     PRIMARY KEY (AssetId, MeasurementDateTime, GateNum)
