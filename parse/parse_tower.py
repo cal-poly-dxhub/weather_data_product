9086f75302bd943635387479d0c4b8a8fb93dadf
chasepeak <chasempeak@gmail.com>
2021-05-13 17:18:21 -0700

    implement + test db connection and insertions

--- parse/parse_tower.py
+++ parse/parse_tower.py
@@ -1,25 +1,19 @@
 import csv
 import sys
 from datetime import datetime
-#from db_config import RDS_HOST, NAME, PASSWORD, DB_NAME
-#import pymysql
+from db_config import RDS_HOST, NAME, PASSWORD, DB_NAME
+import pymysql
 
-csv_file_path = "./sample_data/MetTower.0097.20210414230100.csv"
+csv_file_path = "./MetTower.0097.20210414230100.csv"
 
 tower_instrument = {
     "tower_id": None,
     "measurement_date_time": None
 }
 
-tower_codes = {
-    '2070': 'Average 1-Minute Wind X Component',
-    '2072': 'Average 1-Minute Temperature',
-    '2076': 'Average 1-Minute Relative Humidity'
-    # ...
-}
 def main():
     #------------------------------------ csv --------------------------------------
-    '''
+    
     try:
         conn = pymysql.connect(host=RDS_HOST, user=NAME, password=PASSWORD, database=DB_NAME, connect_timeout=5)
     except Exception as e:
@@ -27,7 +21,7 @@ def main():
         print(str(e))
         sys.exit()
     cur = conn.cursor()
-    '''
+    
 
     with open(csv_file_path, "r") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -40,33 +34,39 @@ def main():
             line_num += 1
             if line_num == 1:
                 tower_instrument['tower_id'] = str(int(row[0]))
+            
             elif line_num == 2:
                 #convert datetime format to expected DB format
                 tower_instrument['measurement_date_time'] = datetime.strftime(datetime.strptime(row[0], "%d/%m/%Y %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
 
 
                 insert_measurement_stmt = "INSERT INTO TowerMeasurements(TowerID, MeasurementDateTime) VALUES(%s, %s)"
+                select_measurement_stmt = "SELECT MAX(MeasurementID) FROM TowerMeasurements;"
                 measurement_data = [tower_instrument['tower_id'], tower_instrument['measurement_date_time']]
 
-                #response = cur.execute(insert_measurement_stmt, measurement_data)
-                #print(response)
-
-                #measurement_id = response[...]
-
-
+                cur.execute(insert_measurement_stmt, measurement_data)
+                cur.execute(select_measurement_stmt)
+                
+                measurement_id = cur.fetchone()[0]
+            
             elif line_num > 2 and len(row) >= 3:
                 try:
-                    insertions.append( [eval(x) for x in row[:3]] )
+                    #validate row[0] is a product code before appending to the insertion set?
+                    insertions.append( row[:3] )
                 except Exception as e:
                     print("exception occurred at line: {}".format(line_num))
                     print(str(e))
+            
 
         insert_products_stmt = "INSERT INTO TowerProductCodeResponse(MeasurementID, ProductCode, HeightMeasurement, Value) VALUES({}, %s, %s, %s)".format(measurement_id)
 
-        #response = pymysql.executemany(insert_products_stmt, insertions)
-        #print(response)
+        response = cur.executemany(insert_products_stmt, insertions)
+        print(response)
 
     csv_file.close()
+
+    conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
