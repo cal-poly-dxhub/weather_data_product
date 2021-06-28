14fad6e78297cb028c87d4d351ee8a1870b7a1c4
Ro-Bert-Web <robertbhs424@gmail.com>
2021-06-28 16:29:51 -0700

    Made ParseAmps create insert statement for sql

--- parse/ParseAmps.py
+++ parse/ParseAmps.py
@@ -1,57 +1,45 @@
+import pymysql
 import sys
 import re
+from datetime import datetime
 
-def parse_line(line, headers):
-    data = {}
-    values = line.split(' ')
-    if (len(values) != len(headers)):
-        print('Error: Not all values are accounted for: ', line)
-        sys.exit()
-    for i in range(len(values)):
-        data[headers[i]] = float(values[i])
-    return data
+def bucket_read(file_name):
+    file = open(file_name, "r")
 
-def parse_file(file_name):
-    file = open(file_name, 'r')
-    lines = re.sub(' +', ' ', file.read()).split('\n')
-    for i in range(len(lines)):
-        lines[i] = lines[i].strip()
+    row_num = 0
+    lines = re.sub(" +", " ", file.read()).split("\n")
+    lines = [line.strip() for line in lines]
+    insert_data_stmt_header = """Insert into Amps2Measurements(FileLocation, MeasurementDatatime,
+        GeometHeight, GeopotHeight, RiseRate, Dir, SpeedFPS, SpeedKnots, Temp, Dew, BP, RH, ABH,
+        DEN, MIR, OIR, VS, Shear, ShrDir, VP, PW, VX, VY, VelErr, Edit) Values """
+    insert_data_stmt_footer = ""
 
-#    headers = lines[3].split(' ')
-    headers = [
-        'sample time',
-        'geomet height',
-        'geopot height',
-        'rise rate',
-        'dir',
-        'speed fps',
-        'speed knots',
-        'temp',
-        'dew',
-        'bp',
-        'rh',
-        'abh',
-        'den',
-        'mir',
-        'oir',
-        'vs',
-        'shear',
-        'shr Dir',
-        'vp',
-        'pw',
-        'vx',
-        'vy',
-        'vel err',
-        'edit'
-    ]
+    measurement_id = None
+    date_time = None
 
-    for line in lines[4:]:
-        if re.search('END_OF_RECORD', line):
+    for row in lines:
+        row_num += 1
+        row_arr = row.split()
+        if row_num == 1:
+            row_arr = row.split("_")
+            measurement_id = row_arr[2]
+            print("Measurement ID: {}".format(measurement_id))
+            date = "/".join(row_arr[4:7])
+            time = ":".join(row_arr[7:])
+            date_time_str = date + " " + time
+            date_time = datetime.strptime(
+                date_time_str, "%Y/%m/%d %H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
+
+        elif re.search("END_OF_RECORD", row): # End of file
             break
-        data = parse_line(line, headers)
-        for header in headers:
-            print(header, ':', data[header], ' ', end='')
-        print()
+        elif row_num >= 5: # Data
+            insert_data_stmt_footer = "({}, {}, {})".format(file_name, date_time, ", ".join(row_arr[1:]))
+            insert_data_stmt_final = insert_data_stmt_header + insert_data_stmt_footer
+            print(insert_data_stmt_final)
+        else:
+            continue
+
+
 
 args = sys.argv[1:]
-parse_file(args[0])
+bucket_read(args[0])
